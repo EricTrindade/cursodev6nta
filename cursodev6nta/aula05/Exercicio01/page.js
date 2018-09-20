63b03dfa785ff573a0bda02a42cee6297681f43a
26146045 <26146045@UNA007-L201-007.serlab.intra>
2018-09-20 15:38:47 -0300

    exercicio02

--- cursodev6nta/aula05/Exercicio01/page.js
+++ cursodev6nta/aula05/Exercicio01/page.js
@@ -2,14 +2,30 @@
  * 
  */
 
-function soma(num1+num2) {
-	var num1=document.getElementById("num1").value;
-	var num2=document.getElementById("num2").value;
-	
-	
+function soma() {
+	var num1 = parseFloat(document.getElementById("num1").value);
+	var num2 = parseFloat(document.getElementById("num2").value);
+	var resultado = num1+num2;
+	document.getElementById("resultado").innerHTML = resultado;
 }
 
+function subtracao() {
+	var num1 = parseFloat(document.getElementById("num1").value);
+	var num2 = parseFloat(document.getElementById("num2").value);
+	var resultado = num1-num2;
+	document.getElementById("resultado").innerHTML = resultado;
+}
 
+function multiplicacao() {
+	var num1 = parseFloat(document.getElementById("num1").value);
+	var num2 = parseFloat(document.getElementById("num2").value);
+	var resultado = num1*num2;
+	document.getElementById("resultado").innerHTML = resultado;
+}
 
-
-
+function divisao() {
+	var num1 = parseFloat(document.getElementById("num1").value);
+	var num2 = parseFloat(document.getElementById("num2").value);
+	var resultado = num1/num2;
+	document.getElementById("resultado").innerHTML = resultado;
+}
---
+++ cursodev6nta/aula05/Exercicio02/page.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+function fazerLogin(){
+	var login=document.getElementById("login").value;
+	var senha=document.getElementById("senha").value;
+
+	if(login=="admin" && senha=="web123"){
+		
+	}
+	
+}
+
+
+
