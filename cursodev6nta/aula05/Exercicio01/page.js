3773c623b156320d9ec9d796adbfa49ee0f828b9
Eric Trindade <Eric Trindade@EricTrindade>
2018-09-19 23:30:41 -0300

    exercicio 01

--- cursodev6nta/aula05/Exercicio01/page.js
+++ cursodev6nta/aula05/Exercicio01/page.js
@@ -2,10 +2,23 @@
  * 
  */
 
-function soma(num1+num2) {
-	var num1=document.getElementById("num1").value;
-	var num2=document.getElementById("num2").value;
-	
+var resultado;
+
+function numeros(){
+	var n1 = document.getElementById("num1").value;
+    var n2 = document.getElementById("num2").value;
+}
+
+function MosResultado(){
+	var elementoResultado = document.getElementById("resultado");
+	elementoResultado.innerHTML = "Resultado: "+ resultado;
+	}
+
+
+function soma() {
+	numeros();
+	resultado = parseFloat(n1)+parseFloat(n2);
+	MosResultado();
 	
 }
 
