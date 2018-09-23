4a566f3788fba1a742f8e41fd0021d6c36079858
Eric Trindade <Eric Trindade@EricTrindade>
2018-09-23 01:07:07 -0300

    Exercicio04 parte1

---
+++ cursodev6nta/aula05/Exercicio04/page.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+function colocarCarrinho(){
+	var produto = document.getElementById("produto").value;
+	var quantidade = document.getElementById("quantidade").value;
+	document.getElementById("tabProduto").value = produto;
+	alert(produto);
+}
+
